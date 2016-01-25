cda581453aae73cef9d57f4c51db24fd04816829
David Landry <davidlandry93@gmail.com>
2016-01-24 21:16:33 -0500

    Now have pregression indicators

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,45 @@
 
+var timer;
+
+function resetTimer() {
+    clearInterval(timer);
+    timer = window.setInterval(scrollOnce, 2000);
+}
 
 function scrollOnce() {
     var carouselItemWidth = document.getElementsByClassName('carousel-item')[0].offsetWidth;
     document.getElementsByClassName('carousel-viewport')[0].scrollLeft += carouselItemWidth;
+    resetTimer();
 }
 
 function scrollBackwardsOnce() {
     var carouselItemWidth = document.getElementsByClassName('carousel-item')[0].offsetWidth;
     document.getElementsByClassName('carousel-viewport')[0].scrollLeft -= carouselItemWidth;
+    resetTimer();
+}
+
+function bullets(pictureIndex, nOfPictures) {
+    stringOfBullets = '<div class="navigation-bullets">';
+    for(var i = 0; i < nOfPictures; i++) {
+        if(i === pictureIndex) {
+            stringOfBullets += '◉';
+        } else {
+            stringOfBullets += '○';
+        }
+    }
+    return stringOfBullets + '</div>';
 }
 
 function prepareCarousel() {
     var items = document.getElementsByClassName('carousel-item');
 
     for(var i = 0; i < items.length; i++) {
-        items[i].innerHTML = '<div class="navigation-button previous-element-button" onclick="scrollBackwardsOnce()">←</div>' +
+        items[i].innerHTML =
+            '<div class="navigation-button previous-element-button"' +
+            ' onclick="scrollBackwardsOnce()">←</div>' +
+            bullets(i, items.length) +
             '<div class="navigation-button next-element-button" onclick="scrollOnce()"><span>→</span></div>';
     }
 
-    window.setInterval(scrollOnce, 2000);
+    resetTimer();
 }
